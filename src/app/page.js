57357c020be0101fe478c795daf3a299e44aa4f6
edberaga <edbetjonnathan@gmail.com>
2024-09-23 11:53:10 +0700

    Remove pricing

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,6 @@ import Services from "./components/services";
 import AgencyTab from "./components/agencyTab";
 import Cta from "./components/cta";
 import Client from "./components/client";
-import Pricing from "./components/pricing";
 import Blogs from "./components/blog";
 import GetInTouch from "./components/getInTuoch";
 import Footer from "./components/footer";
